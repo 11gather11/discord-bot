4320063134d938c206ab8ec10668aff1450a8031
11gather11 <11gather11@gmail.com>
2024-07-01 11:32:34 +0900

    chore: Update main.ts formatting and remove unnecessary semicolons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { Client, GatewayIntentBits } from "discord.js";
-import * as dotenv from "dotenv";
+import { Client, GatewayIntentBits } from 'discord.js'
+import * as dotenv from 'dotenv'
 
 // .envファイルから環境変数を読み込む
-dotenv.config();
+dotenv.config()
 
 // 新しいClientインスタンスを作成
 const client = new Client({
@@ -13,31 +13,31 @@ const client = new Client({
     GatewayIntentBits.GuildPresences,
     GatewayIntentBits.GuildMembers,
   ],
-});
+})
 
 // ボットのトークンを環境変数から取得
-const token = process.env.DISCORD_BOT_TOKEN;
+const token = process.env.DISCORD_BOT_TOKEN
 
 if (!token) {
-  console.error("DISCORD_BOT_TOKEN is not defined in .env file");
-  process.exit(1);
+  console.error('DISCORD_BOT_TOKEN is not defined in .env file')
+  process.exit(1)
 }
 
 // ボットが起動したときに実行されるイベント
-client.once("ready", () => {
-  console.log("Bot is online!");
-});
+client.once('ready', () => {
+  console.log('Bot is online!')
+})
 
 // メッセージが送信されたときに実行されるイベント
-client.on("messageCreate", (message) => {
+client.on('messageCreate', (message) => {
   // ボット自身のメッセージには反応しないようにする
-  if (message.author.bot) return;
+  if (message.author.bot) return
 
   // ユーザーが「!ping」と入力した場合に「Pong!」と応答する
-  if (message.content === "!ping") {
-    message.channel.send("Pong!");
+  if (message.content === '!ping') {
+    void message.channel.send('Pong!')
   }
-});
+})
 
 // ボットをログインさせる
-client.login(token);
+void client.login(token)
