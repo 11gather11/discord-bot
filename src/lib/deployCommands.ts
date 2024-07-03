dc88248ef9f9f33539201ae3103ce3dc2ce8328f
11gather11 <11gather11@gmail.com>
2024-07-03 11:56:55 +0900

    chore: Update .env and main.ts with Discord client and guild IDs

---
+++ src/lib/deployCommands.ts
@@ -0,0 +1,60 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import {
+  Collection,
+  REST,
+  Routes,
+  type CommandInteraction,
+  type RESTPostAPIApplicationCommandsJSONBody,
+} from 'discord.js'
+import * as dotenv from 'dotenv'
+
+// .envファイルから環境変数を読み込む
+dotenv.config()
+
+// コマンドの型定義
+interface Command {
+  data: RESTPostAPIApplicationCommandsJSONBody
+  execute: (interaction: CommandInteraction) => Promise<void>
+}
+
+export const commands = new Collection<string, Command>()
+
+export const deployCommands = async (
+  commandsPath: string,
+  clientId: string,
+  guildId: string,
+  token: string
+): Promise<void> => {
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.ts'))
+
+  const promises = commandFiles.map(async (file) => {
+    const filePath = path.join(commandsPath, file)
+    try {
+      const command = (await import(filePath)) as Command
+      commands.set(command.data.name, command)
+    } catch (error) {
+      console.error(`${file} のコマンドの読み込みエラー:`, error)
+    }
+  })
+
+  await Promise.all(promises)
+  console.log(`${commands.size} 個のコマンドが読み込まれました。`)
+
+  // スラッシュコマンドを登録
+  const rest = new REST({ version: '10' }).setToken(token)
+  const commandData: RESTPostAPIApplicationCommandsJSONBody[] = commands.map(
+    (command) => command.data
+  )
+
+  try {
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commandData,
+    })
+    console.log('スラッシュコマンドが正常に登録されました。')
+  } catch (error) {
+    console.error('コマンドの登録に失敗しました:', error)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import {
 } from 'discord.js'
 import * as dotenv from 'dotenv'
 
-import { commands, deployGlobalCommands } from './lib/deployGlobalCommands' // deployCommands関数とcommandsコレクションをインポート
+import { commands, deployCommands } from './lib/deployCommands' // deployCommands関数とcommandsコレクションをインポート
 
 // .envファイルから環境変数を読み込む
 dotenv.config()
@@ -25,8 +25,9 @@ const client = new Client({
 // ボットのトークンを環境変数から取得
 const token = process.env.DISCORD_BOT_TOKEN
 const clientId = process.env.DISCORD_CLIENT_ID
+const guildId = process.env.DISCORD_GUILD_ID
 
-if (!token || !clientId) {
+if (!token || !clientId || !guildId) {
   console.error('環境変数が設定されていません。')
   process.exit(1)
 }
@@ -42,7 +43,7 @@ client.once(Events.ClientReady, (client): void => {
       console.log(`準備OK! ${client.user.tag} がログインしました。`)
 
       // コマンドを読み込んで登録
-      await deployGlobalCommands(commandsPath, clientId, token)
+      await deployCommands(commandsPath, clientId, guildId, token)
     } catch (error) {
       console.error('コマンドの読み込みと登録に失敗しました:', error)
     }
