4c2913f70ebd442abcbf5118b9e4cb8e2044df77
11gather11 <11gather11@gmail.com>
2025-11-17 17:46:49 +0900

    feat: 不要なコメントを削除

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { Client, GatewayIntentBits } from 'discord.js'
 import { loadCommands } from '@/handlers/commands'
 import { loadEvents } from '@/handlers/events'
 
-// 新しいClientインスタンスを作成
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
